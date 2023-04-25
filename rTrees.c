473ebebb4ae7f083d8c5791179a5f62f5be32e73
dhyey1403 <93812782+dhyey1403@users.noreply.github.com>
2023-04-25 13:19:28 +0530

    function definitions init commit

---
+++ rTrees.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include "rTrees.h"
+
+MBR *createMBR(int minX, int maxX, int minY, int maxY)
+{
+    MBR *rectangle = (MBR *)malloc(sizeof(MBR));
+    rectangle->pairX.minLimit = minX;
+    rectangle->pairX.maxLimit = maxX;
+    rectangle->pairY.minLimit = minY;
+    rectangle->pairY.maxLimit = maxY;
+    return rectangle;
+}
+
+Node *createNode(Entry *parent, rTree *tree)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->noOfEntries = 0;
+    node->isLeaf = 0;
+    node->parent = parent;
+    node->entries = (Entry **)malloc(tree->maxChildren * (sizeof(Entry *)));
+    return node;
+}
+
+Entry *createEntry(MBR *rectangle, Node *child)
+{
+    Entry *entry = (Entry *)malloc(sizeof(Entry));
+    entry->childNode = child;
+    entry->rectangle = rectangle;
+    return entry;
+}
+
+rTree *createRtree(int minchild, int maxchild)
+{
+    rTree *tree;
+    tree->minChildren = minchild;
+    tree->maxChildren = maxchild;
+    tree->start = (Entry *)malloc(sizeof(Entry));
+    tree->start->rectangle = NULL;
+    tree->root = createNode(tree->start, tree);
+    return tree;
+}
